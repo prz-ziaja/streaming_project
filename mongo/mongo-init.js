9d3e01ff0d9301e4c5090216e18e3550a5c0038e
Przemek <prz.ziaja@gmail.com>
2020-12-14 21:37:04 +0100

    mongo fix

--- mongo/mongo-init.js
+++ mongo/mongo-init.js
@@ -1,9 +1,32 @@
-print('Starting configuration')
-db = db.getSiblingDB('api_dev_db')
+print('Starting configuration');
+db = new Mongo().getDB("api_dev_db");
 db.createUser(
     {
         user: 'api_user',
-        pwd: 'pass',
+        pwd: 'raspberry',
+        roles: [{role: 'read',db:'api_dev_db'}]
+    }
+);
+db.createUser(
+    {
+        user: 'backend_user',
+        pwd: 'papay',
         roles: [{role: 'readWrite',db:'api_dev_db'}]
     }
-)
+);
+db.createUser(
+    {
+        user: 'analyser',
+        pwd: 'banana',
+        roles: [{role: 'readWrite',db:'api_dev_db'}]
+    }
+);
+
+db.createCollection( 'photos',
+    {
+      autoIndexId: true,
+      size: 1024,
+      capped: true
+    }
+ );
+ db.photos.insert([{'test':1}])
