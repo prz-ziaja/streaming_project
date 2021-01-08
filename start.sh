dd50ad46d716b39bcc7bdba0864503690e3e1e00
Adam Klekowski <klekowskiadam@gmail.com>
2021-01-08 20:55:42 +0100

    add downloading files from Google Drive

--- start.sh
+++ start.sh
@@ -1,2 +1,8 @@
+wget https://drive.google.com/file/d/1YkOsz9JxGtHRxkqoNqnBAqftILKWDA4j/view\?usp\=sharing
+mv 'view?usp=sharing' car_recognizer/app/model-weights-spectrico-mmr-mobilenet-128x128-344FF72B.pb
+
+wget https://drive.google.com/file/d/1F863SjOpad2_qEXOkyWPyXCyEQZfQmdt/view?usp=sharing
+mv 'view?usp=sharing' car_recognizer/app/yolov3.weights
+
 docker network create my_network
 docker-compose up -d
